33c03eaf5f32c335cbdea55dc204b9c65760b050
Philip Schildkamp <philip.schildkamp@uni-koeln.de>
2023-08-08 11:23:03 +0200

    Add spring boilerplate

---
+++ src/main/java/de/uoc/dh/idh/autodone/AutodoneApplication.java
@@ -0,0 +1,13 @@
+package de.uoc.dh.idh.autodone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutodoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutodoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/uoc/dh/idh/autodone/AutodoneApplicationTests.java
@@ -0,0 +1,13 @@
+package de.uoc.dh.idh.autodone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutodoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
