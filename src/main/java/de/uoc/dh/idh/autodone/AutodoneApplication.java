589c83cb74e3b6b7cd24f9f0c8ac9b58db31d611
Philip Schildkamp <philip.schildkamp@uni-koeln.de>
2024-01-16 22:23:09 +0100

    Statically provide application context and environment

--- src/main/java/de/uoc/dh/idh/autodone/AutodoneApplication.java
+++ src/main/java/de/uoc/dh/idh/autodone/AutodoneApplication.java
@@ -2,12 +2,42 @@ package de.uoc.dh.idh.autodone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication()
-public class AutodoneApplication {
+public class AutodoneApplication extends SpringApplication {
+
+	private static ConfigurableApplicationContext context;
+
+	private static ConfigurableEnvironment environment;
+
+	public static ApplicationContext getContext() {
+		return context;
+	}
+
+	public static Environment getEnvironment() {
+		return environment;
+	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(AutodoneApplication.class, args);
+		new AutodoneApplication().run(args);
+	}
+
+	protected AutodoneApplication() {
+		super(AutodoneApplication.class);
+	}
+
+	@Override()
+	protected void applyInitializers(ConfigurableApplicationContext context) {
+		super.applyInitializers(AutodoneApplication.context = context);
+	}
+
+	@Override()
+	protected void bindToSpringApplication(ConfigurableEnvironment environment) {
+		super.bindToSpringApplication(AutodoneApplication.environment = environment);
 	}
 
 }
