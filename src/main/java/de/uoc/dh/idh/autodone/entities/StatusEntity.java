43ba766a2af8923072422f334f23bfa9bcc91400
tdiepenb <tdiepenb@smail.uni-koeln.de>
2025-01-11 14:21:51 +0100

    reset formatting of StatusEntity

--- src/main/java/de/uoc/dh/idh/autodone/entities/StatusEntity.java
+++ src/main/java/de/uoc/dh/idh/autodone/entities/StatusEntity.java
@@ -1,49 +1,56 @@
 package de.uoc.dh.idh.autodone.entities;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import jakarta.persistence.*;
-import lombok.Data;
+import static jakarta.persistence.CascadeType.ALL;
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.UUID;
 
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
-import static jakarta.persistence.CascadeType.ALL;
-import static jakarta.persistence.FetchType.LAZY;
-import static jakarta.persistence.GenerationType.UUID;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Transient;
+import lombok.Data;
 
 @Data()
 @Entity()
 @JsonNaming(SnakeCaseStrategy.class)
 public class StatusEntity {
 
-    @Id()
-    @GeneratedValue(strategy = UUID)
-    public UUID uuid;
+	@Id()
+	@GeneratedValue(strategy = UUID)
+	public UUID uuid;
 
-    //
+	//
 
-    @ManyToOne(optional = false)
-    public GroupEntity group;
+	@ManyToOne(optional = false)
+	public GroupEntity group;
 
-    @OneToMany(cascade = ALL, fetch = LAZY, mappedBy = "status")
-    public List<MediaEntity> media;
+	@OneToMany(cascade = ALL, fetch = LAZY, mappedBy = "status")
+	public List<MediaEntity> media;
 
-    //
+	//
 
-    @Column(nullable = false)
-    public Instant date;
+	@Column(nullable = false)
+	public Instant date;
 
-    @Column()
-    public String id;
+	@Column()
+	public String id;
 
-    @Column(nullable = false, length = 500)
-    public String status;
+	@Column(nullable = false, length = 500)
+	public String status;
 
-    //
+	//
 
-    @Transient()
-    public List<Exception> exceptions;
+	@Transient()
+	public List<Exception> exceptions;
 
 }
